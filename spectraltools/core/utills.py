40077cd447f8bce7be551bfe046d2ad7a59e7aef
Tom Walker <58395984+ThumbmasWalker@users.noreply.github.com>
2024-01-14 15:46:53 +0000

    Update utills.py

--- spectraltools/core/utills.py
+++ spectraltools/core/utills.py
@@ -16,28 +16,18 @@ def set_layer(model, name, layer):
         pass
     setattr(model, name, layer)
 
-def spectral_linear(model,old_layer=Linear, new_layer=Spectral, verbose=False):
-  i=0
+def spectral_linear(model,old_layer=Linear, new_layer=Spectral, verbose=True):
   for name, module in model.named_modules():
-     ### WIP ###
-      print(i)
-      try: 
-        print(module.layers.named_modules)
-        for name, module2 in module.layers.named_modules:
-          if isinstance(module2, old_layer):
+          if isinstance(module, old_layer):
             # Get current bn layer
-            ol = get_layer(module.layers, name)
+            ol = get_layer(model, name)
             # Create new gn layer
             nl = Sequential(ol, new_layer(ol.out_features))
             # Assign gn
             if verbose:
               print("Swapping {} with {}".format(ol, nl))
             set_layer(model, name, nl)
-      except:
-        print(module.named_modules)
-        
-      
-      i += 1
+
 
 def spectral_conv2d(model, old_layer=Conv2d, new_layer=Spectral, verbose=False):
   for name, module in model.named_modules():
